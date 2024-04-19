b31ae46b461339c8ae2eaade3b4a733e862d28d1
Andrés Banda <andrezh_gln7@live.com>
2024-04-18 23:45:49 -0500

    Add social sharing with facebook and linkedin

--- src/app/components/Social/Social.tsx
+++ src/app/components/Social/Social.tsx
@@ -1,11 +1,31 @@
-import { BiLogoInstagramAlt, BiLogoFacebook, BiDownload } from "react-icons/bi";
+"use client";
+import { BiLogoLinkedin, BiLogoFacebook, BiDownload } from "react-icons/bi";
 import styles from "./social.module.css";
+import { FacebookShareButton, LinkedinShareButton } from "next-share";
+
+const baseURL = "https://hispanicgroup.com";
 
 const Social = () => (
   <div className={styles.social}>
-    <BiLogoInstagramAlt size={40} color={"#6041c4"} />
-    <BiLogoFacebook size={40} color={"#6041c4"} />
-    <BiDownload size={40} color={"#6041c4"} />
+    <FacebookShareButton
+      url={`${baseURL}/caroB10`}
+      quote={"Mira mi nuevo logro"}
+      hashtag={"#nextshare"}
+    >
+      <BiLogoFacebook size={40} color={"#6041c4"} />
+    </FacebookShareButton>
+
+    <LinkedinShareButton url={"https://github.com/next-share"}>
+      <BiLogoLinkedin size={40} color={"#6041c4"} />
+    </LinkedinShareButton>
+
+    <FacebookShareButton
+      url={`${baseURL}/caroB10`}
+      quote={"Mira mi nuevo logro"}
+      hashtag={"#nextshare"}
+    >
+      <BiDownload size={40} color={"#6041c4"} />{" "}
+    </FacebookShareButton>
   </div>
 );
 
